477a65b6d27c232145e7da9c41ac5e0109e2a161
deathsea <coder.deathsea@gmail.com>
2016-07-20 12:16:46 +0800

    train id opr function add

--- app/src/main/java/liuxiang/com/liuxiang/model/TrainID.java
+++ app/src/main/java/liuxiang/com/liuxiang/model/TrainID.java
@@ -1,5 +1,12 @@
 package liuxiang.com.liuxiang.model;
 
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by deathsea on 2016/7/20.
  */
@@ -10,8 +17,23 @@ public class TrainID {
     private String endStationName;
     private String startTime;
     private String arriveTime;
+    private String startDate;
     private int seats;
     private double price;
+    private String targetTable = "trainid";
+    private APPDBHelper APH;
+    private Context CC;
+
+    public TrainID(Context c){APH = new APPDBHelper(c);CC= c;}
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
 
     public int getId() {
         return id;
@@ -75,5 +97,30 @@ public class TrainID {
 
     public void setTrainNo(String trainNo) {
         this.trainNo = trainNo;
+    }
+
+    public List<TrainID> queryTrainID(String from,String to,String date){
+        String[] columns = new String[]{"id","trainNo","startStationName","endStationName",
+                "startTime","arriveTime","startDate","seats","price"};
+        String selection = "startStationName=? and endStationName=? and startDate=?";
+        String[] selectionArgs = new String[]{from,to,date};
+        List<TrainID> result = new ArrayList<TrainID>();
+        SQLiteDatabase db = APH.getReadableDatabase();
+        Cursor queryCursor = db.query(targetTable,columns,selection,selectionArgs,null,null,null);
+        while(queryCursor.moveToNext())
+        {
+            TrainID newTrainID = new TrainID(CC);
+            newTrainID.id = queryCursor.getInt(0);
+            newTrainID.trainNo = queryCursor.getString(1);
+            newTrainID.startStationName = from;
+            newTrainID.endStationName = to;
+            newTrainID.startTime = queryCursor.getString(4);
+            newTrainID.arriveTime = queryCursor.getString(5);
+            newTrainID.startDate = date;
+            newTrainID.seats = queryCursor.getInt(7);
+            newTrainID.price = queryCursor.getDouble(8);
+            result.add(newTrainID);
+        }
+        return result;
     }
 }
