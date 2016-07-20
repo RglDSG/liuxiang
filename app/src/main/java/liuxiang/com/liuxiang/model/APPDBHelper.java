d9f5d8a9170a2f4d883432ea73d2f01281c479f9
deathsea <coder.deathsea@gmail.com>
2016-07-20 18:19:59 +0800

    change to a apk inside databases instead of init databases

--- app/src/main/java/liuxiang/com/liuxiang/model/APPDBHelper.java
+++ app/src/main/java/liuxiang/com/liuxiang/model/APPDBHelper.java
@@ -2,7 +2,15 @@ package liuxiang.com.liuxiang.model;
 
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 /**
  * Created by deathsea on 2016/7/20.
@@ -48,23 +56,65 @@ public final class APPDBHelper extends SQLiteOpenHelper{
     private static String SQL_DELETE_CONTACT = "drop table if exists contact;";
     private static String SQL_DELETE_TRAINID = "drop table if exists trainid;";
     private static String SQL_DELETE_ORDER = "drop table if exists order;";
+    private final static String DATABASE_PATH = "/data/data/liuxiang.com.liuxiang/databases/";
     private final static String DATABASE_NAME = "liuxiang.db";
     private final static int DATABASE_VERSION = 1;
+    private Context thisCotext;
 
-    public APPDBHelper(Context c){super(c,DATABASE_NAME,null,DATABASE_VERSION);}
+    public APPDBHelper(Context c) {
+        super(c,DATABASE_NAME,null,DATABASE_VERSION);
+        thisCotext = c;
+        boolean dbexist = checkDatabase();
+        if (!dbexist) {
+            Log.e("liuxiang Databasehelper","Database doesn't exist");
+            try{copyDatabase();}
+            catch (IOException e){e.printStackTrace();}
+        } else {
+//            Log.e("Database exists");
+//            openDatabase();
+        }
+    }
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-        sqLiteDatabase.execSQL(SQL_CREATE_USER);
-        sqLiteDatabase.execSQL(SQL_CREATE_CONTACTS);
-        sqLiteDatabase.execSQL(SQL_CREATE_TRAINID);
-        sqLiteDatabase.execSQL(SQL_CREATE_ORDER);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-        sqLiteDatabase.execSQL(SQL_DELETE_USER);
-        sqLiteDatabase.execSQL(SQL_DELETE_CONTACT);
-        sqLiteDatabase.execSQL(SQL_DELETE_TRAINID);
-        sqLiteDatabase.execSQL(SQL_DELETE_ORDER);
     }
+
+    private boolean checkDatabase(){
+        boolean checkdb = false;
+        try {
+            String myPath = DATABASE_PATH + DATABASE_NAME;
+            File dbfile = new File(myPath);
+            checkdb = dbfile.exists();
+        } catch(SQLiteException e) {
+            Log.e("liuxiang Databasehelper","Database doesn't exist");
+        }
+        return checkdb;
+    }
+
+    private void copyDatabase()  throws IOException {
+        //Open your local db as the input stream
+        InputStream myinput = thisCotext.getAssets().open(DATABASE_NAME);
+
+        // Path to the just created empty db
+        String outfilename = DATABASE_PATH + DATABASE_NAME;
+
+        //Open the empty db as the output stream
+        OutputStream myoutput = new FileOutputStream(outfilename);
+
+        // transfer byte to inputfile to outputfile
+        byte[] buffer = new byte[1024];
+        int length;
+        while ((length = myinput.read(buffer))>0) {
+            myoutput.write(buffer,0,length);
+        }
+
+        //Close the streams
+        myoutput.flush();
+        myoutput.close();
+        myinput.close();
+    }
+
 }
